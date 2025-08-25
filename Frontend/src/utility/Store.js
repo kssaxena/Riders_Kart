18e0c39cfec3ddaa49be141ffe5827403a14a197
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2025-08-26 01:09:00 +0530

    store initialization

--- Frontend/src/utility/Store.js
+++ Frontend/src/utility/Store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import UserInfoSlice from "./Slice/UserInfoSlice";
 import AllOrderList from "./Slice/AllOrdersSlice";
 import AllAppointmentsSlice from "./Slice/AllAppointmentsSlice";
+import OrderStatusSlice from "./Slice/OrderStatus"; // ✅ import new slice
 
 const store = configureStore({
   reducer: {
     UserInfo: UserInfoSlice,
     AllOrders: AllOrderList,
     AllAppointments: AllAppointmentsSlice,
+    orderStatus: OrderStatusSlice, // ✅ add reducer
   },
 });
 
